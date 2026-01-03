4143711794a391b0ca9c1b03be13f13d7833803d
Adam Clark <adam@eryjus.com>
2026-01-03 14:13:51 -0800

    WIP: refactor visitors into independent sources

--- inc/visitors.hh
+++ inc/visitors.hh
@@ -13,91 +13,11 @@
 
 
 
-//
-// -- This class will be able to visit each symbol structure
-//    ------------------------------------------------------
-class SymbolVisitor {
-public:
-    virtual void Visit(const Symbol &) = 0;
-    virtual void Visit(const TypeSymbol &) = 0;
-    virtual void Visit(const EnumTypeSymbol &) = 0;
-    virtual void Visit(const RecordTypeSymbol &) = 0;
-    virtual void Visit(const DerivedTypeSymbol &) = 0;
-    virtual void Visit(const AccessTypeSymbol &) = 0;
-    virtual void Visit(const IntegerTypeSymbol &) = 0;
-    virtual void Visit(const RealTypeSymbol &) = 0;
-    virtual void Visit(const ArrayTypeSymbol &) = 0;
-    virtual void Visit(const SubtypeSymbol &) = 0;
-    virtual void Visit(const EnumLiteralSymbol  &) = 0;
-    virtual void Visit(const DiscriminantSymbol &) = 0;
-    virtual void Visit(const AttributeSymbol &) = 0;
-    virtual void Visit(const ObjectSymbol &) = 0;
-    virtual void Visit(const ComponentSymbol &) = 0;
-    virtual void Visit(const IncompleteTypeSymbol &) = 0;
-};
+#pragma once
+
+
+#include "visitors/sym-visitor.hh"
+#include "visitors/ast-visitor.hh"
 
 
 
-//
-// -- This class will be used to print the symbol table
-//    -------------------------------------------------
-class SymbolPrinter : public SymbolVisitor {
-private:
-   std::ostream &out;
-
-
-public:
-    explicit SymbolPrinter(std::ostream &o) : out(o) {}
-
-
-public:
-    virtual void Visit(const Symbol &s) override;
-    virtual void Visit(const TypeSymbol &s) override;
-    virtual void Visit(const EnumTypeSymbol &s) override;
-    virtual void Visit(const RecordTypeSymbol &s) override;
-    virtual void Visit(const DerivedTypeSymbol &s) override;
-    virtual void Visit(const AccessTypeSymbol &s) override;
-    virtual void Visit(const IntegerTypeSymbol &s) override;
-    virtual void Visit(const RealTypeSymbol &s) override;
-    virtual void Visit(const ArrayTypeSymbol &s) override;
-    virtual void Visit(const SubtypeSymbol &s) override;
-    virtual void Visit(const EnumLiteralSymbol &s) override;
-    virtual void Visit(const DiscriminantSymbol &s) override;
-    virtual void Visit(const AttributeSymbol &s) override;
-    virtual void Visit(const ObjectSymbol &s) override;
-    virtual void Visit(const ComponentSymbol &s) override;
-    virtual void Visit(const IncompleteTypeSymbol &s) override;
-};
-
-
-
-class ASTVisitor {
-public:
-    virtual void Visit(const SubtypeIndication &) = 0;
-    virtual void Visit(const Name &) { std::cout << "Unimplemented\n"; }
-};
-
-
-
-
-
-class ASTPrinter : public ASTVisitor {
-protected:
-    int depth = 0;
-
-
-public:
-    ASTPrinter(void) { std::cout << "PRINTING THE AST STRUCTURE\n==========================\n"; }
-
-protected:
-
-    void PrintDepth(void) {
-        for (int i = 0; i < depth; i ++) std::cout << ' ';
-    }
-
-
-protected:
-    void PrintRequiredChild(std::string label, ASTNode *child);
-    void PrintOptionalChild(std::string label, ASTNode *child);
-    void Visit(const SubtypeIndication &);
-};
